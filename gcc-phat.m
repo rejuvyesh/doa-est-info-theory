881669ffe7f4f2851e13a1f1a7b84d53516b6c87
rejuvyesh <mail@rejuvyesh.com>
2014-04-03 01:10:54 +0530

    do gcc-phat estimate

---
+++ gcc-phat.m
@@ -0,0 +1,28 @@
+for i = 1:3
+    tau(i) = gcc_phat(x{i}(1,:), x{i}(2,:))
+end
+
+function tau = gcc_phat(sig1, sig2)
+% Find FFT for the signals
+    fft1  = fft(sig1, fftSize(sig1));
+    fft2  = fft(sig2, fftSize(sig2));
+    % Find R(\Tau)
+    G12   = fft1.*conj(fft2);
+    denom = abs(G12);
+    R     = G12./denom;
+
+    % Maximize R ?
+    r     = ifft(R);
+    d1    = real(r);
+    d2    = max(abs(r));
+    tau   = find(abs(r)==d2);
+end
+
+function fftsz = fftSize(sig)
+% Find 2^x such that 2^x>2*nSamples
+    nSamples = length(sig)
+    fftsz = 2;
+    while fftsz < 2*nSamples
+        fftsz = fftsz*2;
+    end
+end
