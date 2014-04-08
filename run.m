5e380f31138c90027650e91d7f33ee4615237d3f
rejuvyesh <mail@rejuvyesh.com>
2014-04-08 16:42:54 +0530

    wave2sig, ,

--- run.m
+++ run.m
@@ -10,11 +10,23 @@ beta     = [0.15 0.30 0.50];      % T60 - Reverberation Time
 snr = 15; % Noise to be added
 
 % Load speech signal
-t = 0:.0001:5;
-speech = sin(2*pi*t); % sine for now
+% t = 0:.0001:5;
+% speech = sin(2*pi*t); % sine for now
+
+% male speech
+fname = 'lalan2.wav';
+[wave, Fs] = wav2sig({fname}, 8000);
+N = numel(wave); 
+dur = N/Fs; % total duration
+t = linspace(0,dur,N);
+speech = wave(t>1 & t<6);
+
+% fs = 8000;
+% fname = {'lalan2.wav'};
+% speech = wav2sig(fname, fs);
 
 %--------- beta = 0.15 ---------
-[sig1 sig2] = generate(c, fs, source, receiver, L, beta(1), speech, snr);
+[sig1, sig2] = generate(c, fs, source, receiver, L, beta(1), speech, snr);
 tau = gcc_phat(sig1, sig2);
 % tau_prime = finddelay(sig1, sig2); % Exists in matlab itself
-[lag I] = mi(sig1, sig2, 10, 4, 2);
+[lag, I] = mi(sig1, sig2, 10, 4, 2);
