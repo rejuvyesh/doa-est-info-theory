d37b14bda52551ddff31151f5757c3f68b64d514
rejuvyesh <mail@rejuvyesh.com>
2014-04-12 20:19:20 +0530

    add lables, legends etc

--- plotres.m
+++ plotres.m
@@ -1,13 +1,13 @@
 % Plots all the plots as presented in the paper
 %
-% Author: Arpit Jangid, Jayesh Kumar Gupta, 2014.
+% Author: Aatmaram Yadav, Arpit Jangid, Jayesh Kumar Gupta, 2014.
 %
 % Contact: Jayesh Kumar Gupta http://rejuvyesh.com
 %          Indian Institute of Technology, Kanpur, India
 
 
 % o is order here
-T60   = [0.15 0.30 0.50];
+T60    = [0.15 0.30 0.50];
 fs    = 44100;
 
 for i =1:3
@@ -19,9 +19,14 @@ for i =1:3
 
     % for plotting the first graph
     j = 2:10;
+    
     color = ['b' 'g' 'r'];
     plot(j,rmse_mi,color(i));
-    hold on;
+    title ('RMSE versus order N for different values of T60');
+    xlabel('Order N');
+    ylabel('RMSE');
+    legend('blue T60=0.15sec','green T60=0.30sec',' red T60=0.50sec');
+    hold on ;
 end
 print -dpng plot1.png
 close;
@@ -35,10 +40,13 @@ for i = 1:2
         L = ceil(T(i)*fs*multiple(k));
         [rmse_mi(k), rmse_gcc(k)] =  run(10,L,T(i));
     end
-
     plot(multiple, rmse_mi, '--');
     hold on;
     plot(multiple, rmse_gcc./100, '-');
+    title ('RMSE of MI and GCC-PHAT systems for varying L shown for T = 0:15 s and T = 0:30 s');
+    xlabel('Block size L');
+    ylabel('RMSE');
+    legend('T60=0.15sec','T60=0.30sec','T60=0.50sec')
     hold on;
 end
 print -dpng plot2.png
@@ -52,9 +60,13 @@ for i = 1:5
     L = ceil(T(i)*fs*0.5);
     [rmse_mi(i), rmse_gcc(i)] =  run(10,L,T(i));
 end
-
+figure;
 plot(T,rmse_mi, '--');
 hold on;
 plot(T,rmse_gcc./100, '-');
+title ('RMSE of MI and GCC-PHAT systems for varying T60');
+xlabel('Order N');
+ylabel('T60 in secs');
+legend('--- MI','___ GCC-PHAT');
 print -dpng plot3.png
-close;
+close;    
