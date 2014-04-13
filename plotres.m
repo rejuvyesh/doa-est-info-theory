628f033ddae662f88a18b03ff3327371956f9c75
rejuvyesh <mail@rejuvyesh.com>
2014-04-13 12:57:31 +0530

    correct legend

--- plotres.m
+++ plotres.m
@@ -25,34 +25,13 @@ for i =1:3
     title ('RMSE versus order N for different values of T60');
     xlabel('Order N');
     ylabel('RMSE');
-    legend('blue T60=0.15sec','green T60=0.30sec',' red T60=0.50sec');
+    legend('blue T60=0.15sec','green T60=0.30sec','red T60=0.50sec');
     hold on ;
 end
 print -dpng plot1.png
 close;
 
 % plot 2
-for i = 2:10
-    multiple(i-1) = 0.1*i;
-end
-for i = 1:2
-    for k = 1:9
-        L = ceil(T60(i)*fs*multiple(k));
-        [rmse_mi(k), rmse_gcc(k)] =  run(10,L,T(i));
-    end
-    plot(multiple, rmse_mi, '--');
-    hold on;
-    plot(multiple, rmse_gcc./100, '-');
-    title ('RMSE of MI and GCC-PHAT systems for varying L shown for T = 0:15 s and T = 0:30 s');
-    xlabel('Block size L');
-    ylabel('RMSE');
-    legend('T60=0.15sec','T60=0.30sec','T60=0.50sec')
-    hold on;
-end
-print -dpng plot2.png
-close;
-
-% plot 3
 clear all; 
 T = [0.1:0.1:0.5];
 fs = 44100;
@@ -65,8 +44,29 @@ plot(T,rmse_mi, '--');
 hold on;
 plot(T,rmse_gcc./100, '-');
 title ('RMSE of MI and GCC-PHAT systems for varying T60');
-xlabel('Order N');
-ylabel('T60 in secs');
-legend('--- MI','___ GCC-PHAT');
+xlabel('RMSE');
+ylabel('T60 (sec)');
+legend('MI','GCC-PHAT');
+print -dpng plot2.png
+close;
+
+% plot 3
+for i = 2:10
+    multiple(i-1) = 0.1*i;
+end
+for i = 1:2
+    for k = 1:9
+        L = ceil(T60(i)*fs*multiple(k));
+        [rmse_mi(k), rmse_gcc(k)] =  run(10,L,T(i));
+    end
+    plot(multiple, rmse_mi, '--');
+    hold on;
+    plot(multiple, rmse_gcc./100, '-');
+    title ('RMSE of MI and GCC-PHAT systems for varying L shown for T = 0:15 s and T = 0:30 s');
+    xlabel('RMSE');
+    ylabel('Block size L');
+    legend('T60=0.30sec GCC-PHAT','T60=0.15sec info theory','T60=0.30sec info theory', 'T60=0.15sec GCC-PHAT')
+    hold on;
+end
 print -dpng plot3.png
-close;    
+close;
